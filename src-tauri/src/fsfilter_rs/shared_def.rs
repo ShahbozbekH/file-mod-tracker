79992fa83c7b46c361d07663c31df8cf8b2661d7
ShahbozbekH <st.hakimov@ufl.edu>
2025-03-28 00:41:45 -0400

    push 2

--- src-tauri/src/fsfilter_rs/shared_def.rs
+++ src-tauri/src/fsfilter_rs/shared_def.rs
@@ -1,5 +1,6 @@
 //! Contains all definitions shared between this user-mode app and the minifilter in order to
 //! communicate properly. Those are C-representation of structures sent or received from the minifilter.
+#[allow(non_local_definitions)]
 
 use std::cmp::Ordering;
 use std::fmt;
